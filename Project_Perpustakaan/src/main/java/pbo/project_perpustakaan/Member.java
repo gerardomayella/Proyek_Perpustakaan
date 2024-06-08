b7db9318f061d7af1212bcce8f79dd1b287623cd
gerardomayella <127300451+gerardomayella@users.noreply.github.com>
2024-06-08 16:29:16 +0700

    update-5

--- Project_Perpustakaan/src/main/java/pbo/project_perpustakaan/Member.java
+++ Project_Perpustakaan/src/main/java/pbo/project_perpustakaan/Member.java
@@ -1,5 +1,6 @@
 package pbo.project_perpustakaan;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Member {
@@ -39,6 +40,7 @@ public class Member {
 
             peminjaman[batasPeminjaman] = new peminjamanKoleksi(koleksi, tanggalPinjam, tanggalKembali);
             batasPeminjaman++;
+
             return "peminjaman berhasil";
         } else {
             return "maaf terjadi kesalahan : mungkin karena batas sudah terpenuhi yaitu 20 koleksi atau anda b atau barang sedang tidak tersedia";
@@ -46,12 +48,28 @@ public class Member {
     }
 
     public String setPengembalianBarang() {
+        Scanner input = new Scanner(System.in);
         if (batasPeminjaman == 0) {
             return "belum ada peminjaman";
         } else {
             for (int i = batasPeminjaman; i >= 0; i--) {
-
+                System.out.println((i + 1) + " " + peminjaman[i]);
             }
+            while (true) {
+                System.out.println("pilih koleksi yang ingin anda kembalikan : ");
+                try {
+                    int pilihan = input.nextInt();
+                    if (pilihan > 0 && pilihan < batasPeminjaman + 1) {
+                        break;
+                    } else {
+                        System.out.println("maaf inputan anda salah");
+                    }
+
+                } catch (InputMismatchException e) {
+                    System.out.println("maaf inputan anda salah");
+                }
+            }
+
         }
     }
 
